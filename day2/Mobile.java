5fff0dc072b5c4cc1a9a7742a9d32622b19fdcfa
Udayalakshmi0501 <132730210+Udayalakshmi0501@users.noreply.github.com>
2023-05-08 21:21:06 +0530

    Add files via upload
    Week2Day2

---
+++ day2/Mobile.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		
+		String mobileBrandName="Samsung Galaxy A33";
+		System.out.println(mobileBrandName);
+		char mobileLogo='S';
+		System.out.println(mobileLogo);
+		short noOfMobilePiece=5;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=336;
+		System.out.println(modelNumber);
+		long mobileImeiNumber=351567422655517L;
+		System.out.println(mobileImeiNumber);
+		float mobilePrice=29999.99F;
+		System.out.println(mobilePrice);
+		boolean isDamaged=true;
+		System.out.println(isDamaged);
+		
+	
+	}
+
+}
---
+++ day2/TwoWheeler.java
@@ -0,0 +1,22 @@
+package week1.day2;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+	
+		int noOfWheels=2;
+		System.out.println(noOfWheels);
+		short noOfMirrors=2;
+		System.out.println(noOfMirrors);
+		long chassisNumber=2451L;
+		System.out.println(chassisNumber);
+		boolean isPunctured=true;
+		System.out.println(isPunctured);
+		String bikeName="Hero Honda";
+		System.out.println(bikeName);
+		double runningKM=1057.5D;
+		System.out.println(runningKM);
+		
+	}
+
+}
